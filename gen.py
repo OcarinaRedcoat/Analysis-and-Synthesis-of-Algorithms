504852cd6a90569423c00252e22b2ff593a67da5
OcarinaRedcoat <98.ricardo.caetano@gmail.com>
2019-03-14 12:29:07 +0000

    Graph

---
+++ gen.py
@@ -0,0 +1,17 @@
+from random import *
+
+N = 100 # randint(1, 10)
+M = 100  # randint(1, 10)
+
+print(N)
+print(M)
+
+E = []
+
+i = 0
+while i < M:
+    x = randint(1, N)
+    y = randint(1, N)
+    if x != y and (x, y) not in E:
+        print(x, y)
+        i += 1

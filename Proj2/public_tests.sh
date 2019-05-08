05d5ef9d619584dbbe32ef85aac8a507a5fb3885
Manuel Goulão <msilvagoulao@gmail.com>
2019-05-08 17:10:58 +0100

    add public_test shell script

---
+++ Proj2/public_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# chmod +x test.sh
+# cd student-tests
+# ./test.sh ../program.out
+
+cd ./student-tests
+for in in `ls | grep T`
+do  
+    echo "Running $in"
+    name=`basename ${in}`
+    in="$name/input"
+    out="$name/output"
+    myout="$name/my.out"
+    mydiff="$name/diff"
+    program="../p2"
+    $program < $in > $myout
+    diff $out $myout > $mydiff
+    if [[ -s ${mydiff} ]]; then
+        echo "FAIL: $in "
+    else
+        echo "Passed :) "
+    fi
+done

cc734bd65f68f290e8e93adda77f3faa19f10ace
Manuel Goulão <msilvagoulao@gmail.com>
2019-05-11 15:24:54 +0100

    remove prints improve memory usage

--- Proj2/p2.cpp
+++ Proj2/p2.cpp
@@ -21,13 +21,10 @@ class ResidualArch {
   Vertex *origin_vertex;
   bool canBeCut;
   bool augmentingArch;
-  bool processed;
 
-  // ResidualArch *pair;
+  ResidualArch() : flux(0) {}
 
-  ResidualArch() : flux(0), processed(false) {}
-
-  ResidualArch(int cap, Vertex *dest) : flux(0), processed(false) {
+  ResidualArch(int cap, Vertex *dest) : flux(0) {
     capacity = cap;
     dest_vertex = dest;
   }
@@ -42,15 +39,11 @@ class ResidualArch {
   int getCapacity() { return capacity; }
 
   void addFlux(int f) {
-    // printf("%d\n", capacity);
     flux += f;
     capacity = capacity - f;
-    // printf("f: %d\n", flux);
-    // printf("%d\n", capacity);
   }
 
   int getFlux() {
-    // printf("%d\n", flux);
     return flux;
   }
 };
@@ -58,7 +51,6 @@ class ResidualArch {
 class Vertex {
  private:
   int id;
-  int realId;
 
  public:
   int height;
@@ -68,25 +60,19 @@ class Vertex {
 
   bool station;
   int production;
-  bool cutVisited;
   bool canBeCut;
   bool augmentingVertex;
   bool visited;
-  bool processed;
-
-  Vertex *predecessorVertex;  // if the  predecessor vertex is the source the
-                              // vertex is a supplier if not is a station
-  ResidualArch *predecessorArch;
 
   list<ResidualArch *> backArches;
   list<ResidualArch *> arches;
   queue<ResidualArch *> archeLessHeight;
 
-  Vertex() : height(0), excess(0), flow(0), processed(false) {}
+  Vertex() : height(0), excess(0), flow(0) {}
 
-  Vertex(int i) : height(0), excess(0), flow(0), processed(false) { id = i; }
+  Vertex(int i) : height(0), excess(0), flow(0) { id = i; }
 
-  Vertex(int i, int h) : excess(0), processed(false) {
+  Vertex(int i, int h) : excess(0) {
     id = i;
     height = h;
   }
@@ -94,10 +80,6 @@ class Vertex {
   void setId(int i) { id = i; }
 
   int getId() { return id; }
-
-  void setRealId(int i) { realId = i; }
-
-  int getRealId() { return realId; }
 
   void addFlux(int f) { flow += f; }
 
@@ -120,9 +102,7 @@ class Vertex {
 
     for (ResidualArch *temp : arches) {
       if (temp->dest_vertex->height < height && temp->getCapacity() > 0) {
-        /*         printf("&&&&& id : %d altura: %d\n",
-         * temp->dest_vertex->getId(), temp->dest_vertex->height);
-         */
+
         archeLessHeight.push(temp);
       }
     }
@@ -163,7 +143,7 @@ class Graph {
     vertexes = new Vertex[suppliers + (2 * stations)];
     source = new Vertex(0);
     hyper = new Vertex(1);  // sink
-    hyper->processed = true;
+
 
     for (int i = 1; i <= suppliers; i++) {
       vertexes[i - 1].setId(i + 1);
@@ -176,7 +156,6 @@ class Graph {
       vertexes[i + suppliers - 1].setStation();
       vertexes[i + suppliers + stations - 1].setId(i + suppliers + stations +
                                                    1);
-      vertexes[i + suppliers + stations - 1].setRealId(i + suppliers + 1);
       scanf("%d", &vertexes[i + suppliers - 1].production);
       vertexes[i + suppliers - 1].addResidualArch(
           vertexes[i + suppliers - 1].production,
@@ -216,9 +195,8 @@ class Graph {
     for (int i = 0; i < suppliers + (2 * stations); i++) {
       if (!vertexes[i].arches.empty())
         for (ResidualArch *arch : vertexes[i].arches) {
-          printf("orin: %d -- dest: %d -- fluxo: %d --- processed: %d\n",
-                 vertexes[i].getId(), arch->dest_vertex->getId(), arch->flux,
-                 arch->processed);
+          printf("orin: %d -- dest: %d -- fluxo: %d\n",
+                 vertexes[i].getId(), arch->dest_vertex->getId(), arch->flux);
         }
     }
   }
@@ -252,17 +230,13 @@ class PushRelabel {
 
   void init_pre_flow(Graph g) {
     g.source->height = maxHeight;
-    g.source->processed = true;
 
     for (ResidualArch *arch : g.source->arches) {
       int capacity = arch->getCapacity();
       if (capacity > 0) {
         arch->addFlux(capacity);
         g.source->excess -= capacity;
-        // printf("%d\n", arch->getCapacity());
         arch->dest_vertex->excess += capacity;
-        // printf("%d\n", arch->dest_vertex->excess);
-        arch->dest_vertex->processed = true;
         arch->dest_vertex->addFlux(capacity);
         stack.push(arch->dest_vertex);
       }
@@ -273,8 +247,6 @@ class PushRelabel {
     int min_height = INT_MAX;
     for (ResidualArch *arc : u->arches) {  // Front Edges
       if (arc->getCapacity() > 0 && arc->dest_vertex->height <= min_height) {
-        /* printf(">>>>>>>%d, height: %d\n", arc->dest_vertex->getId(),
-         * arc->dest_vertex->height); */
         min_height = arc->dest_vertex->height;
       }
     }
@@ -283,17 +255,9 @@ class PushRelabel {
       if (arc->origin_vertex->height <= min_height && arc->flux > 0) {
         min_height = arc->origin_vertex->height;
       }
-      /* printf("dest: %d, height: %d, min_height: %d\n",
-         arc->origin_vertex->getId(), arc->origin_vertex->height, min_height);
-       */
     }
 
-    /*  assert(u->excess > 0);
-     assert(u->height <= min_height); */
-    /*         printf("antes %d\n", min_height);
-     */
     u->height = min_height + 1;
-    /* printf("minha aluta %d\n", u->height); */
 
     u->updateArchLessQueue();
   }
@@ -303,27 +267,19 @@ class PushRelabel {
     if (arc->dest_vertex->getId() == u->getId()) {  // Push back
       d = min(u->excess, arc->flux);
       arc->addFlux(-d);
-      /* printf("??????? capa %d, d %d \n", arc->getCapacity(), d); */
       arc->origin_vertex->excess += d;
       if (arc->origin_vertex->getId() != 0) {
-        /* printf("|?|?|?|?|| id %d\n", arc->origin_vertex->getId());
-        printf("u->excess %d, arc->flux %d, arc->getCapacity() %d, DDD: %d\n",
-               u->excess, arc->flux, arc->getCapacity(), d); */
         stack.push(arc->origin_vertex);
       }
     } else {  // Push front
       d = min(u->excess, arc->getCapacity());
-      /* printf("##### capa %d, d %d \n", arc->getCapacity(), d); */
       arc->addFlux(d);
       arc->dest_vertex->excess += d;
       if (arc->dest_vertex->getId() != 1) {
-        /* printf("|#|#|#|#|| id %d\n", arc->origin_vertex->getId()); */
         stack.push(arc->dest_vertex);
-        /* printf("///// ALTURA %d \n", u->height); */
       }
     }
     u->excess -= d;
-    /* printf("**** %d\n", u->excess); */
 
     firstElem = stack.front();
   }
@@ -331,16 +287,11 @@ class PushRelabel {
   void discharge(Vertex *u) {
     u->updateArchLessQueue();
     while (u->excess > 0) {
-      /* printf("+++ +id: %d, excess: %d\n", u->getId(), u->excess); */
       if (u->archeLessHeight.empty()) {
         relabel(u);
-        /* printf("id %d\n", u->getId()); */
       } else {
         ResidualArch *arc = u->archeLessHeight.front();
-        /*  printf("---- meu %d, orig: %d, dest %d, minha altura: %d, orig
-           height %d\n", u->getId(), arc->origin_vertex->getId(),
-                arc->dest_vertex->getId(), u->height,
-                arc->origin_vertex->height); */
+
         u->archeLessHeight.pop();
 
         if (arc->getCapacity() > 0 || arc->dest_vertex->getId() == u->getId()) {
@@ -348,13 +299,9 @@ class PushRelabel {
                u->height > arc->dest_vertex->height) ||
               (arc->dest_vertex->getId() == u->getId() &&
                u->height > arc->origin_vertex->height)) {
-            /* printf("puuuuuusshhh\n"); */
             push(u, arc);
           }
-        } /* else {
-          printf("popoopopopopo\n");
-          u->archeLessHeight.pop();
-        } */
+        } 
       }
     }
   }
@@ -363,7 +310,6 @@ class PushRelabel {
     maxHeight = g.suppliers + (2 * g.stations) + 2;
 
     init_pre_flow(g);
-    /* printf("------------------------\n"); */
     while (!stack.empty()) {
       firstElem = stack.front();
 
@@ -378,25 +324,16 @@ class PushRelabel {
     graph.source->visited = false;
     graph.hyper->visited = false;
 
-    graph.source->predecessorVertex = NULL;
-    graph.hyper->predecessorVertex = NULL;
-
-    graph.source->predecessorArch = NULL;
-    graph.hyper->predecessorArch = NULL;
-
     for (int i = 0; i < graph.suppliers + graph.stations; i++) {
       graph.vertexes[i].visited = false;
-      graph.vertexes[i].cutVisited = false;
       graph.vertexes[i].canBeCut = true;
-      graph.vertexes[i].predecessorVertex = NULL;
-      graph.vertexes[i].predecessorArch = NULL;
     }
   }
 
   void resetVisits() {
     for (int i = 0; i < graph.suppliers + (2 * graph.stations); i++) {
       if (!graph.vertexes[i].augmentingVertex)
-        graph.vertexes[i].cutVisited = false;
+        graph.vertexes[i].visited = false;
     }
   }
 
@@ -407,22 +344,15 @@ class PushRelabel {
     stack.push_front(graph.source);
     graph.source->visited = true;
 
-    while (graph.hyper->predecessorVertex == NULL && !stack.empty()) {
+    while (!stack.empty()) {
       Vertex *temp = stack.front();
       stack.pop_front();
-      /*       if (temp->augmentingVertex && temp->getId() > graph.suppliers +
-         1) { augmentingStations->insert(temp->getId());
-              // printf("---id %d\n", temp->getId());
-            }
-            if (temp->augmentingVertex) {
-              continue;
-            } */
+
       if (temp->augmentingVertex) {
         continue;
       }
       for (ResidualArch *arch : temp->arches) {
-        /* printf("----orig %d, dest %d\n", arch->origin_vertex->getId(),
-                 arch->dest_vertex->getId()); */
+
         if (arch->augmentingArch) {
           if (arch->origin_vertex->isStation()) {
             augmentingStations.insert(temp->getId());
@@ -452,13 +382,10 @@ class PushRelabel {
   void markNextVertices(Vertex *newCutVertex) {
     list<Vertex *> stack;
     stack.push_front(newCutVertex);
-    /* printf("id %d mark\n", newCutVertex->getId()); */
     for (ResidualArch *arch : newCutVertex->arches) {
       if (arch->dest_vertex->getId() != 1 && arch->getCapacity() == 0) {
-        /* printf("----orig %d, dest %d\n", arch->origin_vertex->getId(),
-               arch->dest_vertex->getId()); */
         arch->dest_vertex->canBeCut = false;
-        arch->dest_vertex->cutVisited = true;
+        arch->dest_vertex->visited = true;
         arch->dest_vertex->augmentingVertex = false;
       }
       arch->augmentingArch = false;
@@ -467,13 +394,10 @@ class PushRelabel {
   }
 
   void findGraphCutDFSUtil(Vertex *vertex) {
-    vertex->cutVisited = true;
-    /* printf("orig id %d, %d, flow: %d cap: %d, \n", vertex->getId(),
-           graph.suppliers + 1, vertex->flow, vertex->production); */
+    vertex->visited = true;
     if (vertex->augmentingVertex) return;
     if (vertex->getId() > 1 && vertex->getId() <= graph.suppliers + 1 &&
         vertex->flow == vertex->production) {
-      /* printf("vertex augmenint\n"); */
       vertex->augmentingVertex = true;
       resetVisits();
       markNextVertices(vertex);
@@ -481,28 +405,21 @@ class PushRelabel {
       return;
     }
     for (ResidualArch *arch : vertex->backArches) {
-      /* printf("orig id %d, dest: %d cap: %d, \n",
-         arch->origin_vertex->getId(), arch->dest_vertex->getId(),
-         arch->getCapacity()); */
+
       if (!arch->augmentingArch) {
         if (arch->getCapacity() == 0 && arch->origin_vertex->getId() != 0 &&
             arch->canBeCut && !arch->origin_vertex->isStation()) {
-          /* printf("augmenting arch\n"); */
           arch->augmentingArch = true;
         } else if (arch->getCapacity() == 0 &&
                    arch->origin_vertex->getId() != 0 &&
                    arch->origin_vertex->isStation() &&
                    arch->origin_vertex->canBeCut) {
           resetVisits();
-          /* printf("---orig %d, dest %d\n", arch->origin_vertex->getId(),
-                 arch->dest_vertex->getId()); */
           arch->augmentingArch = true;
           markNextVertices(vertex);
           findGraphCutDFSUtil(graph.hyper);
-          /*printf("is vertex station id: %d\n", arch->origin_vertex->getId());
-           */
         } else if (arch->origin_vertex->getId() != 0 &&
-                   !arch->origin_vertex->cutVisited) {
+                   !arch->origin_vertex->visited) {
           findGraphCutDFSUtil(arch->origin_vertex);
         }
       }
